85fd8082dc51811ad6fbe704169dfae527dacba6
Oscar Montero hinojosa <omontero@c2r3s4.42malaga.com>
2022-04-21 18:01:12 +0200

    1

---
+++ cursus_42/0.0_libft/first_libc.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   first_libc.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omontero <omontero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/18 18:58:48 by omontero          #+#    #+#             */
+/*   Updated: 2022/04/21 16:40:01 by omontero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_isalpha(int n)
+{
+	if ((n >= 'a' && n <= 'z') || (n >= 'A' && n <= 'Z'))
+		return (n);
+	return (0);
+}
+
+int	ft_isdigit(int n)
+{
+	if (n >= '0' && n <= '9')
+		return (n);
+	return (0);
+}
+
+int	ft_isalnum(int n)
+{
+	if (ft_isalpha || ft_isdigit)
+		return (1);
+	return (0);
+}
+
+int	ft_isascii(int n)
+{
+	if (n >= 0 && n <= 127)
+		return (n);
+	return (0);
+}
+
+int	isprintf(int n)
+{
+	if (n >= 32 && n <= 126)
+		return (1);
+	return (0);
+}
---
+++ cursus_42/0.0_libft/libft.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omontero <omontero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/07 09:26:06 by omontero          #+#    #+#             */
+/*   Updated: 2022/04/21 16:23:04 by omontero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+
+int	ft_isalpha(int n);
+int	ft_isdigit(int n);
+int	ft_isalnum(int n);
+int	ft_isascii(int n);
+
+
+#endif
