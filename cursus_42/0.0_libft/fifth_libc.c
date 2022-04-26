d5cf1ecdb817abdeb17a3a21eb9de188cbbcd5c6
OscarW1ld3 <wilde.txt@gmail.com>
2022-04-26 10:09:56 +0200

    L26042022 1009

--- cursus_42/0.0_libft/fifth_libc.c
+++ cursus_42/0.0_libft/fifth_libc.c
@@ -1,13 +1,92 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   first_libc.c                                       :+:      :+:    :+:   */
+/*   fifth_libc.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: omontero <omontero@student.42.fr>          +#+  +:+       +#+        */
+/*   By: oscar <oscar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 18:58:48 by omontero          #+#    #+#             */
-/*   Updated: 2022/04/21 10:02:25 by omontero         ###   ########.fr       */
+/*   Updated: 2022/04/26 09:23:38 by oscar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	ft_check_little(char *p, const char *little, const size_t len)
+{
+	size_t	x;
+	size_t	i;
+	int		tmp;
+
+	x = (size_t)len;
+	i = 0;
+	tmp = 0;
+	while (little[i] && x)
+	{
+		if (p[i] != little[i])
+			tmp = 1;
+		i++;
+		x--;
+	}
+	if (!little[i])
+		return (tmp);
+	return (1);
+}
+
+char	*ft_nptrnnptr(const char *big, const char *little, size_t len)
+{
+	char	*p;
+
+	p = (char *)big;
+	if (*little == '\0')
+		return (p);
+	while (*p && len)
+	{
+		if (*p == *little)
+		{
+			if (!ft_check_little(p, little, len))
+				return (p);
+		}
+		len--;
+		p++;
+	}
+	return (NULL);
+}
+
+/* ************************************************* */
+// Atoi malo, arreglar || Maybe already fixed -> To check
+const int	ft_neg(int num, int x)
+{
+	if (x % 2 == 1)
+	{
+		num = num * -1;
+	}
+	return (num);
+}
+
+int	ft_atoi(const char *nptr)
+{
+	int	x;
+	int	i;
+	int	num;
+
+	i = 0;
+	num = 0;
+	x = 0;
+	while ((nptr[i] >= '\t' && nptr[i] <= '\r') || nptr[i] == ' ')
+	{
+		i++;
+	}
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			x++;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		num = num * 10 + (nptr[i] - 48);
+		i++;
+	}
+	return (ft_neg(num, x));
+}
