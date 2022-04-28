f5dc77b1fd5b7395ca7ea88bfd3ac9742fd2f3f8
OscarW1ld3 <wilde.txt@gmail.com>
2022-04-28 19:40:47 +0200

    L 28-04-2022 19:40

--- cursus_42/0.0_libft/libft/first_add.c
+++ cursus_42/0.0_libft/libft/first_add.c
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   first_add.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: omontero <omontero@student.42.fr>          +#+  +:+       +#+        */
+/*   By: oscar <oscar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 11:17:37 by omontero          #+#    #+#             */
-/*   Updated: 2022/04/28 19:02:22 by omontero         ###   ########.fr       */
+/*   Updated: 2022/04/28 19:39:55 by oscar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
@@ -50,6 +51,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	str[size] = '\0';
 	return (str);
 }
+*/
 
 // Funciones auxiliares de ft_strtrim
 /*
@@ -66,7 +68,7 @@ char	*ft_remfirst(const char *s, const char *set)
 	char	*str;
 	int		x;
 	int		cut;
-	int		i;
+	size_t	i;
 
 	cut = 0;
 	str = ft_strdup(s);
@@ -84,49 +86,57 @@ char	*ft_remfirst(const char *s, const char *set)
 			cut = 1;
 		str++;
 	}
+	str--;
 	return (ft_strdup(str));
 }
 
 char	*ft_remlast(const char *s, const char *set)
 {
 	char	*str;
+	char	*str2;
 	int		x;
 	int		cut;
-	int		i;
+	size_t	i;
 
 	str = ft_strdup(s);
+	str2 = &str[ft_strlen(str) - 1];
+	cut = 0;
+	printf("%c", str[1]);
 	while (*str && !cut)
 	{
 		i = 0;
 		x = 0;
 		while (i < ft_strlen(set) && !x)
 		{
-			if (*str == set[i])
+			if (*str2 == set[i])
 				x = 1;
 			i++;
 		}
 		if (!x)
 			cut = 1;
-		str++;
+		str2--;
 	}
+	str2++;
+	str2 = '\0';
+	str
 	return (ft_strdup(str));
 }
 
 char	*ft_strtrim(const char *s1, char const *set)
 {
-	size_t	size;
 	char	*str;
 
 	str = ft_remfirst(s1, set);
 	str = ft_remlast(str, set);
+
 	if (!str)
 		return (NULL);
-	
+	return (str);
 }
 
 int	main(void)
 {
-	char	s1[] = "hola me llamo oscar";
+	char	s1[] = "abchola me llamo oscarabc";
 	char	set[] = "abc";
 
 	printf("%s", ft_strtrim(s1, set));
