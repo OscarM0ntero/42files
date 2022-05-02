34107965792f47993a96dc609ea1b8f42b2bba0d
OscarW1ld3 <wilde.txt@gmail.com>
2022-05-03 01:07:02 +0200

    L 03-05-2022 01:16

--- cursus_42/0.0_libft/libft/first_add.c
+++ cursus_42/0.0_libft/libft/first_add.c
@@ -6,7 +6,7 @@
 /*   By: oscar <oscar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 11:17:37 by omontero          #+#    #+#             */
-/*   Updated: 2022/04/29 00:15:06 by oscar            ###   ########.fr       */
+/*   Updated: 2022/05/03 01:05:22 by oscar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,3 +50,111 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	str[size] = '\0';
 	return (str);
 }
+
+char	*ft_strtrim(const char *s1, char const *set)
+{
+	char			*str;
+	unsigned int	start;
+	unsigned int	end;
+
+	if (!s1 || !set)
+		return (ft_strdup(s1));
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (ft_strchr(set, s1[start]))
+		start++;
+	while (ft_strchr(set, s1[end]))
+		end--;
+	str = ft_substr(s1, start + 1, end - start + 1);
+	if (!str)
+		return (NULL);
+	return (str);
+}
+
+/* int	main(void)
+{
+	char	s1[] = "abcacb hola abbcaabc";
+	char	set[] = "abc";
+
+	printf("%s", ft_strtrim(s1, set));
+} */
+
+static size_t	ft_count_words(char const *s, char c)
+{
+	size_t	i;
+	size_t	n_words;
+
+	n_words = 0;
+	i = 0;
+	while (s[i] == c)
+		i++;
+	while (s[i])
+	{
+		while (s[i] != c && s[i])
+			i++;
+		if ((s[i] == c || s[i] == '\0') && s[i - 1] != c)
+			n_words++;
+		while (s[i] == c && s[i])
+			i++;
+	}
+	return (n_words);
+}
+
+static void	ft_assign_next_coords(char const *s, size_t *start, size_t *end, char c)
+{
+	*start = *end;
+	while (s[*start] == c && s[*start])
+		start++;
+	*end = *start;
+	while (s[*end] != c && s[*end])
+		end++;
+}
+// "  hola soy  oscar "
+static int	ft_assign_words(char const *s, char c, char **list, size_t n_words)
+{
+	size_t	start;
+	size_t	end;
+	size_t	i;
+	char	*word;
+
+	i = 0;
+	start = 0;
+	end = 0;
+	ft_assign_next_coords(s, &start, &end, c);
+	while (i < n_words)
+	{
+		word = (char *)malloc((end - start) * sizeof(char));
+		if (!word)
+			return (1);
+		word = ft_substr(s, start, end - start);
+		list[i] = word;
+		free(word);
+		ft_assign_next_coords(s, &start, &end, c);
+		i++;
+	}
+	word = (char *)malloc(1 * sizeof(char));
+	word = NULL;
+	list[n_words] = word;
+	free (word);
+	return (0);
+}
+// " hola  soy oscar"
+char	**ft_split(char const *s, char c)
+{
+	size_t	n_words;
+	char	**list;
+
+	n_words = ft_count_words(s, c);
+	list = (char **)malloc((n_words + 1) * sizeof(char *));
+	if (ft_assign_words(s, c, list, n_words) || !list)
+		return (NULL);
+	return (list);
+}
+
+int	main(void)
+{
+	char	str[] = " hola soy oscar  ";
+
+	printf("%s\n", ft_split(str, ' ')[0]);
+}
+// Deberia funcionar, toca depurar errores (Seg. Fault)
--- cursus_42/0.0_libft/libft/second_add.c
+++ cursus_42/0.0_libft/libft/second_add.c
@@ -3,80 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   second_add.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: omontero <omontero@student.42.fr>          +#+  +:+       +#+        */
+/*   By: oscar <oscar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 11:17:37 by omontero          #+#    #+#             */
-/*   Updated: 2022/04/29 13:17:49 by omontero         ###   ########.fr       */
+/*   Updated: 2022/05/03 00:18:01 by oscar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* static char	*ft_remfirst(const char *s, const char *set)
-{
-	char	*str;
-	int		x;
-	int		cut;
-	size_t	i;
-
-	cut = 0;
-	str = ft_strdup(s);
-	while (*str && !cut)
-	{
-		i = 0;
-		x = 0;
-		while (i < ft_strlen(set) && !x)
-		{
-			if (*str == set[i])
-				x = 1;
-			i++;
-		}
-		if (!x)
-			cut = 1;
-		str++;
-	}
-	str--;
-	return (ft_strdup(str));
-}
-
-static char	*ft_remlast_end_finder(char *str2, const char *set)
-{
-	int		cut;
-	size_t	i;
-	int		x;
-
-	cut = 0;
-	while (*str2 && !cut)
-	{
-		i = 0;
-		x = 0;
-		while (i < ft_strlen(set) && !x)
-		{
-			if (*str2 == set[i])
-				x = 1;
-			i++;
-		}
-		if (!x)
-			cut = 1;
-		str2--;
-	}
-	str2 += 2;
-	return (str2);
-}
-
-static char	*ft_remlast(const char *s, const char *set)
-{
-	char	*str;
-	char	*str2;
-
-	str = ft_strdup(s);
-	str2 = &str[ft_strlen(str) - 1];
-	str2 = ft_remlast_end_finder(str2, set);
-	ft_bzero(str2, ft_strlen(str2));
-	return (ft_strdup(str));
-} */
-
-char	*ft_strtrim(const char *s1, char const *set)
+/* char	*ft_strtrim(const char *s1, char const *set)
 {
 	char			*str;
 	unsigned int	start;
@@ -94,12 +30,12 @@ char	*ft_strtrim(const char *s1, char const *set)
 	if (!str)
 		return (NULL);
 	return (str);
-}
+} */
 
-int	main(void)
+/* int	main(void)
 {
-	char	s1[] = "abcacbhola abbcaabc";
+	char	s1[] = "abcacb hola abbcaabc";
 	char	set[] = "abc";
 
 	printf("%s", ft_strtrim(s1, set));
-}
+} */
