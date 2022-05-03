f5cb70d319791833529139f92918acf0bb48f5d9
OscarW1ld3 <wilde.txt@gmail.com>
2022-05-03 14:52:37 +0200

    L 03-05-2022 14:52

--- cursus_42/0.0_libft/libft/fourth_add.c
+++ cursus_42/0.0_libft/libft/fourth_add.c
@@ -6,7 +6,7 @@
 /*   By: oscar <oscar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 11:17:37 by omontero          #+#    #+#             */
-/*   Updated: 2022/05/03 12:08:51 by oscar            ###   ########.fr       */
+/*   Updated: 2022/05/03 14:51:06 by oscar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,19 +36,14 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int	i;
-	char			*p;
 
+	if (!s || !f)
+		return ;
 	i = 0;
-	if (!s || (!s && !f))
-		return (ft_strdup(""));
-	else if (!f)
-		return (ft_strdup(s));
 	while (s[i])
 	{
-		p = &s[i];
-		s[i] = (char)((*f)(i, p));
+		f(i, s + i);
 		i++;
 	}
-	return (s);
 }
 // fix type conversion
