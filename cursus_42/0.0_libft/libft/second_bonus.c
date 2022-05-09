137522d94f4e637b19d109e0a5c67175755406e6
OscarW1ld3 <wilde.txt@gmail.com>
2022-05-09 09:04:57 +0200

    09-05-2022 09:04

--- cursus_42/0.0_libft/libft/second_bonus.c
+++ cursus_42/0.0_libft/libft/second_bonus.c
@@ -6,7 +6,7 @@
 /*   By: oscar <oscar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 09:26:06 by omontero          #+#    #+#             */
-/*   Updated: 2022/05/05 09:37:23 by oscar            ###   ########.fr       */
+/*   Updated: 2022/05/09 09:04:22 by oscar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	
+	if (!lst || !del)
+		return ;
+	del(lst->content);
+	free(lst);
+}
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+
+	if (!lst || !del)
+		return ;
+	while (*lst)
+	{
+		tmp = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = tmp;
+	}
 }
