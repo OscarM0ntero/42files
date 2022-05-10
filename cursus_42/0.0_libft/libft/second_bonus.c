92a27ce47a55e891f546696675c7651b746f4fc5
OscarW1ld3 <wilde.txt@gmail.com>
2022-05-10 09:07:34 +0200

    L 10-05-2022 09:07

--- cursus_42/0.0_libft/libft/second_bonus.c
+++ cursus_42/0.0_libft/libft/second_bonus.c
@@ -6,7 +6,7 @@
 /*   By: oscar <oscar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 09:26:06 by omontero          #+#    #+#             */
-/*   Updated: 2022/05/10 08:58:49 by oscar            ###   ########.fr       */
+/*   Updated: 2022/05/10 09:06:53 by oscar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,27 +51,27 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*first;
-	t_list	*new;
+	t_list	*tmp;
 
 	if (!f || !del)
 		return (NULL);
 	first = NULL;
 	while (lst)
 	{
-		new = ft_lstnew((*f)(lst->content));
-		if (!new)
+		tmp = ft_lstnew((*f)(lst->content));
+		if (!tmp)
 		{
 			while (first)
 			{
-				new = first->next;
+				tmp = first->next;
 				(*del)(first->content);
 				free(first);
-				first = new;
+				first = tmp;
 			}
 			lst = NULL;
 			return (NULL);
 		}
-		ft_lstadd_back(&first, new);
+		ft_lstadd_back(&first, tmp);
 		lst = lst->next;
 	}
 	return (first);
