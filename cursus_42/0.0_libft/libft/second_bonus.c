c7bf77b61b1c5b8d4783f08aa7f4e13149bb2665
OscarW1ld3 <wilde.txt@gmail.com>
2022-05-10 10:02:10 +0200

    L 10-05-2022 10:02

--- cursus_42/0.0_libft/libft/second_bonus.c
+++ cursus_42/0.0_libft/libft/second_bonus.c
@@ -6,7 +6,7 @@
 /*   By: oscar <oscar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 09:26:06 by omontero          #+#    #+#             */
-/*   Updated: 2022/05/10 09:06:53 by oscar            ###   ########.fr       */
+/*   Updated: 2022/05/10 09:32:37 by oscar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,32 +47,37 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 		lst = tmp;
 	}
 }
-
+/*
+Itera la lista lst y aplica f al content de cada
+nodo. Crea la lista resultante de aplicar f a cada
+nodo y la devuelve. del se usa para eliminar
+contenido de un nodo si fuera necesario.
+*/
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*first;
+	t_list	*list;
 	t_list	*tmp;
 
 	if (!f || !del)
 		return (NULL);
-	first = NULL;
+	list = NULL;
 	while (lst)
 	{
 		tmp = ft_lstnew((*f)(lst->content));
 		if (!tmp)
 		{
-			while (first)
+			while (list)
 			{
-				tmp = first->next;
-				(*del)(first->content);
-				free(first);
-				first = tmp;
+				tmp = list->next;
+				(*del)(list->content);
+				free(list);
+				list = tmp;
 			}
 			lst = NULL;
 			return (NULL);
 		}
-		ft_lstadd_back(&first, tmp);
+		ft_lstadd_back(&list, tmp);
 		lst = lst->next;
 	}
-	return (first);
+	return (list);
 }
