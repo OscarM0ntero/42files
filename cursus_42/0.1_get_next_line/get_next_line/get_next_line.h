023220a695492db3f2c1ff10df8bd6ce89db4e19
OscarW1ld3 <wilde.txt@gmail.com>
2022-05-16 23:10:58 +0200

    L 16-05-2022 23:10

---
+++ cursus_42/0.1_get_next_line/get_next_line/get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oscar <oscar@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/07 09:26:06 by omontero          #+#    #+#             */
+/*   Updated: 2022/05/16 23:00:40 by oscar            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <string.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+// get_next_line.c
+int			ft_isalpha(int n);
+int			ft_isdigit(int n);
+int			ft_isalnum(int n);
+#endif
