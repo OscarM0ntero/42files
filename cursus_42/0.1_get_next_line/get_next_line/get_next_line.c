7a0603b85e6e9d148c98018f0e0f3ce982cd2292
Oscar Montero hinojosa <omontero@c2r3s4.42malaga.com>
2022-05-23 10:57:58 +0200

    42 23-05-2022 10:57 -> Avances en get_next_line, LIBFT aprobado(+125) :)

--- cursus_42/0.1_get_next_line/get_next_line/get_next_line.c
+++ cursus_42/0.1_get_next_line/get_next_line/get_next_line.c
@@ -6,88 +6,60 @@
 /*   By: omontero <omontero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 16:53:32 by omontero          #+#    #+#             */
-/*   Updated: 2022/05/18 19:09:00 by omontero         ###   ########.fr       */
+/*   Updated: 2022/05/23 10:56:30 by omontero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+void	*ft_calloc(size_t count, size_t size)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	str = (char *)malloc(count * size);
+	if (str == 0)
+		return (0);
+	while (i < count * size)
+	{
+		str[i] = 0;
+		i++;
+	}
+	return ((void *)str);
+}
+
+// "hola me llamo oscar \nque pasa por aqui?"
 static char	*ft_save_buffers(int fd, char *s)
 {
-	char	*tmp_buff;
+	char	*buff;
 	int		read_output;
 
-	tmp_buff = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!tmp_buff)
+	buff = (char *)ft_calloc((BUFFER_SIZE + 1), sizeof(char));
+	if (!buff)
 		return (NULL);
 	read_output = 1;
-	while (!ft_strchr(tmp_buff, '\n') && read_output != 0)
+	while (!ft_strchr(buff, '\n') && read_output != 0)
 	{
-		read_output = read(fd, tmp_buff, BUFFER_SIZE);
+		read_output = read(fd, buff, BUFFER_SIZE);
 		if (read_output == -1)
 		{
-			free(tmp_buff);
+			free (buff);
 			return (NULL);
 		}
-		tmp_buff[read_output] = '\0';
-		s = ft_strjoin(s, tmp_buff);
+		buff[read_output] = 0;
+		s = ft_strjoin(s, buff);
 	}
-	free(tmp_buff);
+	free (buff);
 	return (s);
 }
 
-static char	*ft_line(char *s)
+static char	*ft_get_line(char *s)
 {
 	char	*line;
-	int		i;
 
-	if (!*s)
-		return (NULL);
-	i = 0;
-	while (s[i] && s[i] != '\n')
-		i++;
-	line = (char *)malloc((i + 2) * sizeof(char));
-	if (!s)
-		return (NULL);
-	i = 0;
-	while (s[i] && s[i] != '\n')
-	{
-		line[i] = s[i];
-		i++;
-	}
-	if (s[i] == '\n')
-	{
-		line[i] = s[i];
-		i++;
-	}
-	line[i] = '\0';
+	//antes de acabar, dejar en (s) el texto sobrante tras el '\n'
 	return (line);
-}
-
-static char	*ft_last_buff(char *s)
-{
-	int		i;
-	int		j;
-	char	*extra;
-
-	i = 0;
-	while (s[i] && s[i] != '\n')
-		i++;
-	if (!s[i])
-	{
-		free(s);
-		return (NULL);
-	}
-	extra = (char *)malloc(sizeof(char) * (ft_strlen(s) - i + 1));
-	if (!s)
-		return (NULL);
-	i++;
-	j = 0;
-	while (s[i])
-		extra[j++] = s[i++];
-	extra[j] = '\0';
-	free(s);
-	return (extra);
 }
 
 char	*get_next_line(int fd)
@@ -100,20 +72,17 @@ char	*get_next_line(int fd)
 	s = ft_save_buffers(fd, s);
 	if (!s)
 		return (NULL);
-	line = ft_line(s);
+	line = ft_get_line(s);
 	if (!line)
 		return (NULL);
-	s = ft_last_buff(s);
-	if (!s)
-		return (NULL);
-	return (line);
+	return (s);
 }
 
-/* int	main(void)
+int	main(void)
 {
 	int	fd;
 
 	fd = open("file.txt", O_RDONLY);
 	printf("%s", get_next_line(fd));
 	close(fd);
-}  */
+}
---
+++ cursus_42/0.1_get_next_line/get_next_line/get_next_line_copy2.c
@@ -0,0 +1,122 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_copy2.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omontero <omontero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/18 16:53:32 by omontero          #+#    #+#             */
+/*   Updated: 2022/05/23 10:48:53 by omontero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+/* static char	*ft_save_buffers(int fd, char *s)
+{
+	char	*tmp_buff;
+	int		read_output;
+
+	if (*s && ft_strchr(s, '\n'))
+		while (*s != '\n')
+			s++;
+	tmp_buff = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!tmp_buff)
+		return (NULL);
+	read_output = 1;
+	while (!ft_strchr(tmp_buff, '\n') && read_output != 0)
+	{
+		read_output = read(fd, tmp_buff, BUFFER_SIZE);
+		if (read_output == -1)
+		{
+			free(tmp_buff);
+			return (NULL);
+		}
+		tmp_buff[read_output] = '\0';
+		s = ft_strjoin(s, tmp_buff);
+	}
+	free(tmp_buff);
+	return (s);
+}
+
+static char	*ft_line(char *s)
+{
+	char	*line;
+	int		i;
+
+	if (!*s)
+		return (NULL);
+	i = 0;
+	while (s[i] && s[i] != '\n')
+		i++;
+	line = (char *)malloc((i + 2) * sizeof(char));
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (s[i] && s[i] != '\n')
+	{
+		line[i] = s[i];
+		i++;
+	}
+	if (s[i] == '\n')
+	{
+		line[i] = s[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+static char	*ft_last_buff(char *s)
+{
+	int		i;
+	int		j;
+	char	*extra;
+
+	i = 0;
+	while (s[i] && s[i] != '\n')
+		i++;
+	if (!s[i])
+	{
+		free(s);
+		return (NULL);
+	}
+	extra = (char *)malloc(sizeof(char) * (ft_strlen(s) - i + 1));
+	if (!s)
+		return (NULL);
+	i++;
+	j = 0;
+	while (s[i])
+		extra[j++] = s[i++];
+	extra[j] = '\0';
+	free(s);
+	return (extra);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*s;
+	char		*line;
+
+	if (fd <= 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	s = ft_save_buffers(fd, s);
+	if (!s)
+		return (NULL);
+	line = ft_line(s);
+	if (!line)
+		return (NULL);
+	s = ft_last_buff(s);
+	if (!s)
+		return (NULL);
+	return (line);
+} */
+
+/* int	main(void)
+{
+	int	fd;
+
+	fd = open("file.txt", O_RDONLY);
+	printf("%s", get_next_line(fd));
+	close(fd);
+}  */
