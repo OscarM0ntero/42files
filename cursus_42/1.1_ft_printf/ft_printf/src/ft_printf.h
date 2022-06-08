b76f7e28d4e407722206146da3041f86a517bf99
OscarW1ld3 <wilde.txt@gmail.com>
2022-06-09 00:45:18 +0200

    L 09-06-2022 00:44 -> ft_printf: Casi terminado, pasar test de fsoares que no los pasa en el portatil

--- cursus_42/1.1_ft_printf/ft_printf/src/ft_printf.h
+++ cursus_42/1.1_ft_printf/ft_printf/src/ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: omontero <omontero@student.42.fr>          +#+  +:+       +#+        */
+/*   By: oscar <oscar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 13:08:08 by omontero          #+#    #+#             */
-/*   Updated: 2022/06/03 13:10:12 by omontero         ###   ########.fr       */
+/*   Updated: 2022/06/08 21:50:38 by oscar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,24 @@
 
 # include <stdarg.h>
 # include <unistd.h>
+# include "../libft/libft.h"
 
+//		ft_printf.c
 int		ft_printf(char const *string, ...);
 
+//		ft_printf_utils.c
+void	ft_putstr(char *str);
+int		ft_printchar(char c);
+int		ft_printstring(char *str);
+int		ft_printnumber(int n);
+int		ft_printpercent(void);
+
+//		ft_print_hex.c
+int		ft_printhex(unsigned int n, const char type);
+
+//		ft_print_pointer.c
+int		ft_printpointer(unsigned long long p);
+
+//		ft_print_unsigned.c
+int		ft_printunsigned(unsigned int n);
 #endif
