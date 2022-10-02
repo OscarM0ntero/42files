be6a90ae0b5c749e9a9fba7c06be090f428b85f7
OscarW1ld3 <94902365+OscarW1ld3@users.noreply.github.com>
2022-10-02 02:09:20 +0200

    LM 02-10-2022 02:09

--- cursus_42/2.0.1_push_swap/push_swap/src/push_swap.c
+++ cursus_42/2.0.1_push_swap/push_swap/src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: oscar <oscar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 19:05:48 by omontero          #+#    #+#             */
-/*   Updated: 2022/06/28 14:27:50 by oscar            ###   ########.fr       */
+/*   Updated: 2022/10/02 02:08:46 by oscar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 //	Esta funcion asigna los valores introduciodos por consola
 //	al stack A de inicio :)
-void assign_values(char ***argv, t_stack *a, t_data data)
+
+void	assign_values(char ***argv, t_stack *a, t_data data)
 {
 	int	i;
 
 	i = 0;
-	*a->size = data.total_amount_of_numbers;
-	*a->stk = ft_calloc(*a->size, sizeof(t_value));
+	a->size = data.total_amount_of_numbers;
+	a->stk = ft_calloc(a->size, sizeof(t_value));
 	while (i < data.total_amount_of_numbers)
 	{
 		a->stk[i].val = ft_atoi(argv[i + 1]);
@@ -30,20 +31,20 @@ void assign_values(char ***argv, t_stack *a, t_data data)
 }
 
 //	Esta funcion simplemente imprime por pantalla el stack que le pasemos
-void read_stack(t_stack x)
+void	read_stack(t_stack x)
 {
 	int	i;
 
 	i = 0;
 	while (i < x.size)
 	{
-		printf("Pos: %d		Value: %d\n", x.stk[i].val,x.stk[i].val);
+		printf("Pos: %d		Value: %d\n", x.stk[i].val, x.stk[i].val);
 		i++;
 	}
 }
 
 //	Busca el mayor numero dentro del Stack
-int search_higher(t_stack x)
+int	search_higher(t_stack x)
 {
 	int	i;
 	int	higher_number_found;
@@ -59,7 +60,7 @@ int search_higher(t_stack x)
 	return (higher_number_found);
 }
 
-int search_lower(t_stack x)
+int	search_lower(t_stack x)
 {
 	int	i;
 	int	lower_number_found;
@@ -79,7 +80,6 @@ t_stack	organize(t_stack a, t_stack b, t_data data)
 {
 	while (data.lowest_number_in_order != data.lowest_number)
 	{
-		
 	}
 	return (a);
 }
@@ -87,9 +87,9 @@ t_stack	organize(t_stack a, t_stack b, t_data data)
 //	./push_swap <nums separados por ' '>
 int	main(int argc, char **argv)
 {
-	t_stack a;
-	t_stack b;
-	t_data data;
+	t_stack	a;
+	t_stack	b;
+	t_data	data;
 
 	data.total_amount_of_numbers = argc - 1;
 	data.numbers_in_order = 0;
@@ -99,10 +99,6 @@ int	main(int argc, char **argv)
 	data.highest_number = search_higher(a);
 	data.lowest_number = search_lower(a);
 	data.lowest_number_in_order = data.highest_number;
-
 	a = organize(a, b, data);
-
-
 	return (0);
 }
-
