1fbeada49a107b288c3408a61bce7180e3a0eaa8
Oscar Montero hinojosa <omontero@c3r2s2.42malaga.com>
2022-10-17 13:54:15 +0200

    17-10-2022 13:54 minitalk: archivos creados

--- cursus_42/2.1_minitalk/client.c
+++ cursus_42/2.1_minitalk/client.c
@@ -1,27 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: omontero <omontero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/18 18:58:48 by omontero          #+#    #+#             */
-/*   Updated: 2022/05/19 11:43:45 by omontero         ###   ########.fr       */
+/*   Created: 2022/06/02 19:05:48 by omontero          #+#    #+#             */
+/*   Updated: 2022/10/17 13:14:36 by omontero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 
-void	ft_lstclear(t_list **lst, void (*del)(void *))
-{
-	t_list	*tmp;
-
-	if (!lst || !del)
-		return ;
-	while (*lst)
-	{
-		tmp = (*lst)->next;
-		ft_lstdelone(*lst, del);
-		*lst = tmp;
-	}
-}
