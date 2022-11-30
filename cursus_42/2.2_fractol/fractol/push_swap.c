9cb5a00d6d8d3c6a593d7837a42ba5ada0d3aa74
Oscar Montero hinojosa <omontero@c2r2s4.42malaga.com>
2022-11-30 15:40:44 +0100

    push swap finsished. fractol started

---
+++ cursus_42/2.2_fractol/fractol/push_swap.c
@@ -0,0 +1,248 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omontero <omontero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/02 19:05:48 by omontero          #+#    #+#             */
+/*   Updated: 2022/11/25 16:49:43 by omontero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+//	Esta funcion asigna los valores introduciodos por consola
+//	al stack A de inicio :)
+/**
+ * 
+*/
+int	assign_values(char **argv, t_stack *a, t_data *data)
+{
+	int	i;
+
+	i = 0;
+	a->size = data->total_amount_of_numbers;
+	a->stk = ft_calloc(a->size, sizeof(t_value));
+	while (i < data->total_amount_of_numbers)
+	{
+		if (!(argv[i + 1][0] >= 48 && argv[i + 1][0] <= 57) && !((argv[i
+					+ 1][0] == '-') && (argv[i + 1][1] >= 48 && argv[i
+					+ 1][1] <= 57)))
+			return (1);
+		a->stk[i].val = ft_atoi_long(argv[i + 1], data);
+		if (data->error)
+			return (1);
+		a->stk[i].pos = i;
+		i++;
+	}
+	return (0);
+}
+
+void	organize_2(t_stack *a, t_data *data)
+{
+	if (a->stk[0].val > a->stk[1].val)
+		ft_sa(a, 1);
+	data->numbers_in_order = 2;
+	data->highest_number_dis = search_lower(*a);
+	data->lowest_number_ord = data->highest_number_dis;
+}
+
+//	Devuelve 1 si A esta ordenado, 2 si hay orden pero empieza en medio,
+//	0 si no hay orden
+int	order(t_stack *a)
+{
+	int	i;
+	int	j;
+	int	x;
+
+	if ((search_num_pos(*a, search_lower(*a)) != 0 && search_higher_pos(*a)
+			!= search_num_pos(*a, search_lower(*a)) - 1) || (search_num_pos(*a,
+				search_lower(*a)) == 0 && search_higher_pos(*a) != a->size - 1))
+		return (0);
+	else
+	{
+		i = a->size;
+		j = search_num_pos(*a, search_lower(*a));
+		while (i--)
+		{
+			x = 1;
+			if (j == a->size - 1)
+				x = -(a->size - 1);
+			if (a->stk[j].val > a->stk[j + x].val && a->stk[j].val != s_h(*a))
+				return (0);
+			j += x;
+		}
+	}
+	if (search_num_pos(*a, search_lower(*a)) == 0)
+		return (1);
+	return (2);
+}
+
+void	organize(t_stack *a, t_stack *b, t_data *data)
+{
+	if (order(a) == 2)
+		return (order_a(a, data));
+	else if (order(a) == 0)
+	{
+		while (data->total_amount_of_numbers != data->numbers_in_order
+			&& order(a) != 1)
+		{
+			if (order(a) == 2)
+				order_a(a, data);
+			else if (data->total_amount_of_numbers == 2)
+				organize_2(a, data);
+			else if (data->total_amount_of_numbers == 3)
+				organize_3(a, data);
+			else
+			{
+				rotate_extract_x_higher(a, b, data);
+				if (data->total_amount_of_numbers < 20)
+					organize_3(a, data);
+				extract_in_order(a, b, data);
+				if (data->total_amount_of_numbers < 20)
+					order_a(a, data);
+			}
+		}
+	}
+}
+
+// 63 89 94 55 31 54 36 39 33 60 27 76 3 17 88 0 67 22 25 64
+//	Hacer free en cada instruccion como he hecho en PUSH y seguir con memdetect
+int	main(int argc, char **argv)
+{
+	t_stack	a;
+	t_stack	b;
+	t_data	data;
+
+	data.error = 0;
+	data.total_amount_of_numbers = argc - 1;
+	if (data.total_amount_of_numbers < 20)
+		data.x = data.total_amount_of_numbers - 4;
+	else if (data.total_amount_of_numbers < 400)
+		data.x = data.total_amount_of_numbers / 4.5;
+	else if (data.total_amount_of_numbers < 1200)
+		data.x = data.total_amount_of_numbers / 7.69;
+	else
+		data.x = data.total_amount_of_numbers / 20;
+	data.numbers_in_order = 0;
+	if (assign_values(argv, &a, &data) || ft_check_rep(&a, &data))
+		return (free (a.stk), free (b.stk), write(1, "Error\n", 6));
+	data.highest_number = s_h(a);
+	data.lowest_number = search_lower(a);
+	data.highest_number_dis = data.highest_number;
+	data.lowest_number_ord = data.highest_number;
+	organize(&a, &b, &data);
+	return (free(a.stk), free(b.stk), 0);
+}
+
+//read_stack(a, b);
+/*
+
+int	main(void)
+{
+	t_stack	a;
+	t_stack	b;
+	t_data	data;
+	int		argc = 21;
+	char	**argv;
+
+	argv = ft_calloc(21, sizeof(char *));
+	argv[0] = ft_strdup("push_swap");
+	argv[1] = ft_strdup("63");
+	argv[2] = ft_strdup("89");
+	argv[3] = ft_strdup("94");
+	argv[4] = ft_strdup("55");
+	argv[5] = ft_strdup("31");
+	argv[6] = ft_strdup("54");
+	argv[7] = ft_strdup("36");
+	argv[8] = ft_strdup("39");
+	argv[9] = ft_strdup("33");
+	argv[10] = ft_strdup("60");
+	argv[11] = ft_strdup("27");
+	argv[12] = ft_strdup("76");
+	argv[13] = ft_strdup("3");
+	argv[14] = ft_strdup("17");
+	argv[15] = ft_strdup("88");
+	argv[16] = ft_strdup("0");
+	argv[17] = ft_strdup("67");
+	argv[18] = ft_strdup("22");
+	argv[19] = ft_strdup("25");
+	argv[20] = ft_strdup("64");
+	data.error = 0;
+	data.total_amount_of_numbers = argc - 1;
+	if (data.total_amount_of_numbers < 20)
+		data.x = data.total_amount_of_numbers;
+	else if (data.total_amount_of_numbers < 400)
+		data.x = data.total_amount_of_numbers / 5;
+	else if (data.total_amount_of_numbers < 1200)
+		data.x = data.total_amount_of_numbers / 10;
+	else
+		data.x = data.total_amount_of_numbers / 20;
+	data.numbers_in_order = 0;
+	if (assign_values(argv, &a, &data) || ft_check_rep(&a, &data))
+	{
+		free (argv[0]);
+		free (argv[1]);
+		free (argv[2]);
+		free (argv[3]);
+		free (argv[4]);
+		free (argv[5]);
+		free (argv[6]);
+		free (argv[7]);
+		free (argv[8]);
+		free (argv[9]);
+		free (argv[10]);
+		free (argv[11]);
+		free (argv[12]);
+		free (argv[13]);
+		free (argv[14]);
+		free (argv[15]);
+		free (argv[16]);
+		free (argv[17]);
+		free (argv[18]);
+		free (argv[19]);
+		free (argv[20]);
+		free (argv[21]);
+		free (argv);
+		free (a.stk);
+		free (b.stk);
+		return (write(1, "Error\n", 6));
+	}
+	data.highest_number = s_h(a);
+	data.lowest_number = search_lower(a);
+	data.highest_number_dis = data.highest_number;
+	data.lowest_number_ord = data.highest_number;
+	organize(&a, &b, &data);
+	free (argv[0]);
+	free (argv[1]);
+	free (argv[2]);
+	free (argv[3]);
+	free (argv[4]);
+	free (argv[5]);
+	free (argv[6]);
+	free (argv[7]);
+	free (argv[8]);
+	free (argv[9]);
+	free (argv[10]);
+	free (argv[11]);
+	free (argv[12]);
+	free (argv[13]);
+	free (argv[14]);
+	free (argv[15]);
+	free (argv[16]);
+	free (argv[17]);
+	free (argv[18]);
+	free (argv[19]);
+	free (argv[20]);
+	free (argv[21]);
+	free (argv);
+	free (a.stk);
+	free (b.stk);
+	return (0);
+}
+
+//read_stack(a, b);
+
+
+*/
---
+++ cursus_42/2.2_fractol/fractol/push_swap.h
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omontero <omontero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/03 13:08:08 by omontero          #+#    #+#             */
+/*   Updated: 2022/11/23 11:57:44 by omontero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../libft/libft.h"
+# include <unistd.h>
+
+//		Structs
+typedef struct s_value
+{
+	int		val;
+	int		pos;
+}			t_value;
+
+typedef struct s_stack
+{
+	t_value	*stk;
+	int		size;
+}			t_stack;
+
+typedef struct s_data
+{
+	int		highest_number_dis;
+	int		lowest_number_ord;
+	int		numbers_in_order;
+	int		highest_number;
+	int		lowest_number;
+	int		total_amount_of_numbers;
+	int		x;
+	int		error;
+}			t_data;
+
+//	push_swap.c
+int			order(t_stack *a);
+
+//	push_swap_utils.c
+void		ft_add_to_stack(t_stack *stack, int new_val, int pos);
+void		ft_remove_from_stack(t_stack *stack, int pos);
+void		read_stack(t_stack a, t_stack b);
+
+//	push_swap_push.c
+void		ft_pa(t_stack *a, t_stack *b);
+void		ft_pb(t_stack *a, t_stack *b);
+
+//	push_swap_swap.c
+void		ft_sa(t_stack *a, int print);
+void		ft_sb(t_stack *b, int print);
+void		ft_ss(t_stack *a, t_stack *b);
+
+//	push_swap_rotate.c
+void		ft_ra(t_stack *a, int print);
+void		ft_rb(t_stack *b, int print);
+void		ft_rr(t_stack *a, t_stack *b);
+
+//	push_swap_reverse_rotate.c
+void		ft_rra(t_stack *a, int print);
+void		ft_rrb(t_stack *b, int print);
+void		ft_rrr(t_stack *a, t_stack *b);
+
+//	push_swap_sorter.c
+void		rotate_extract_10_higher(t_stack *a, t_stack *b, t_data *data);
+void		extract_in_order(t_stack *a, t_stack *b, t_data *data);
+void		realloc_a(t_stack *a, t_data *data);
+void		order_a(t_stack *a, t_data *data);
+
+//	push_swap_sorter_large.c
+void		rotate_extract_100_higher(t_stack *a, t_stack *b, t_data *data);
+
+//	push_swap_sorter_generic.c
+void		rotate_extract_x_higher(t_stack *a, t_stack *b, t_data *data);
+void		organize_3(t_stack *a, t_data *data);
+
+//	push_swap_sorter_utils.c
+int			s_h(t_stack x);
+int			search_higher_pos(t_stack x);
+int			search_lower(t_stack x);
+int			search_num_pos(t_stack x, int val);
+int			search_higher_und_x(t_stack x, int max);
+
+//	push_swap_checkval
+long		ft_atoi_long(const char *nptr, t_data *data);
+int			ft_check_rep(t_stack *a, t_data *data);
+
+#endif
