f77936af457670d4838eb5c9b05b851b7a6b36b7
Oscar Montero hinojosa <omontero@c2r10s1.42malaga.com>
2022-12-01 19:01:36 +0100

    fractol -> inicio

---
+++ cursus_42/2.2_fractol/fractol/fractol.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omontero <omontero@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/01 16:27:58 by omontero          #+#    #+#             */
+/*   Updated: 2022/12/01 18:11:16 by omontero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include "libft/libft.h"
+# include "minilibx_opengl/mlx.h"
+//# include "MLX42/include/MLX42/MLX42.h"
+
+//		Structs
+// typedef struct s_data
+// {
+// }			t_data;
+
+#endif
